c9339ab976bbeb7d9c69277d18f36603a75f98f0
YuriiSokol <yura357117@gmail.com>
2023-04-08 13:28:39 +0200

    1

--- src/index.js
+++ src/index.js
@@ -6,3 +6,45 @@ const input = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 const DEBOUNCE_DELAY = 300;
+
+input.addEventListener(
+  'input',
+  debounce(() => {
+    onInput(input.value.trim());
+  }, DEBOUNCE_DELAY)
+);
+
+const clearResult = () => {
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
+};
+
+const checkCountriesQuantity = req => {
+  if (req.length > 10) {
+    clearResult();
+    return Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  }
+  if (req.length < 10 && req.length > 1) return render10Countries(req);
+  if (req.length === 1) return renderOneCountry(req);
+};
+
+const onInput = input => {
+  if (/[0-9]/.test(input)) {
+    Notiflix.Notify.failure('Please enter correct country name');
+    return;
+  }
+  if (input === '') {
+    clearResult();
+    return;
+  }
+  fetchCountries(input)
+    .then(req => {
+      checkCountriesQuantity(req);
+    })
+    .catch(() => {
+      clearResult();
+      Notiflix.Notify.failure('Oops, there is no country with that name.');
+    });
+};
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,11 @@
+export { fetchCountries };
+
+const fetchCountries = countryName => {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`
+  ).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+  });
+};

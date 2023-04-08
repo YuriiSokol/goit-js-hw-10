b4c8e6f7a2c8a01a860746fdeb034ccad012e9d9
YuriiSokol <yura357117@gmail.com>
2023-04-08 13:25:46 +0200

    imp

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './css/styles.css';
-
+import Notiflix from 'notiflix';
+import { fetchCountries } from './js/fetchCountries';
+import debounce from 'lodash.debounce';
+const input = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 const DEBOUNCE_DELAY = 300;

c6996d0ad696c6e38079e8fb06f1348b80ab5f1b
YuriiSokol <yura357117@gmail.com>
2023-04-08 13:31:54 +0200

    qqqqq

--- src/index.js
+++ src/index.js
@@ -48,3 +48,43 @@ const onInput = input => {
       Notiflix.Notify.failure('Oops, there is no country with that name.');
     });
 };
+
+const render10Countries = req => {
+  clearResult();
+  countryList.insertAdjacentHTML(
+    'beforeend',
+    req
+      .map(elem => {
+        const svgLink = elem.flags.svg;
+        const name = elem.name.official;
+        return `<li class='country-list__element' > <img class='country-elem__image' src='${svgLink}' alt="country flag"  height='50' width='50' "/> <p class='country-elem__text'>${name}</p> </li>`;
+      })
+      .join('')
+  );
+};
+const renderOneCountry = req => {
+  clearResult();
+  const element = req
+    .map(elem => {
+      const svgLink = elem.flags.svg;
+      const name = elem.name.common;
+      let languagesList = '';
+      const languagesArr = Object.values(elem.languages);
+      languagesArr.map(element => {
+        if (languagesArr.indexOf(element) === languagesArr.length - 1) {
+          languagesList = languagesList + element;
+          return;
+        }
+        languagesList = languagesList + element + ', ';
+      });
+
+      return `<div class="country-info__header"><img src='${svgLink}' alt="country flag"  height='50' width='50' class="country-info__flag" "/> <p class="country-info__name" >${name}</p></div>
+       <ul class="country-info__details-list">
+          <li class="detail-list__elem"><span class="detail-list__name" >Capital: </span> <span class="detail-list__value" >${elem.capital}</span></li>
+          <li class="detail-list__elem"><span class="detail-list__name">Population: </span> <span class="detail-list__value">${elem.population}</span></li>
+          <li class="detail-list__elem"><span class="detail-list__name">Languages: </span> <span class="detail-list__value">${languagesList}</span></li>
+       </ul>`;
+    })
+    .join('');
+  countryInfo.insertAdjacentHTML('afterbegin', element);
+};
